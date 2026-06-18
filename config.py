9f2977fdaef8a79f2209ad2b294fa1483dadfb3d
MohammedDev-yt <animeworldday2005@gmail.com>
2026-06-18 12:45:35 +0530

    config.py

---
+++ config.py
@@ -0,0 +1,32 @@
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+import os
+
+API_ID = os.environ.get("API_ID","")
+API_HASH = os.environ.get("API_HASH","")
+BOT_TOKEN = os.environ.get("BOT_TOKEN","")
+
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+DATABASE_URL = os.environ.get("DATABASE_URL","")
+BOT_USERNAME = os.environ.get("BOT_USERNAME","AU_Auto_Delete_bot") # Without @
+FORCE_SUB_CHANNEL = os.environ.get("FORCE_SUB_CHANNEL", "Anime_UpdatesAU")  # without @
+
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+PORT = int(os.environ.get("PORT", 8080))  # for Flask binding (Koyeb)
+OWNER_ID = int(os.environ.get("OWNER_ID", "7284759394"))  # your Telegram user ID
+
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
